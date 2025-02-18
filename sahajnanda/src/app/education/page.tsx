de39fcd4d9084b3e952ea52b366ff6f5bb0e3ddd
SahajNanda <89496230+SahajNanda@users.noreply.github.com>
2025-02-17 21:52:41 -0500

    education

---
+++ sahajnanda/src/app/education/page.tsx
@@ -0,0 +1,7 @@
+export default function Education() {
+  return (
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Education</h1>
+    </div>
+  );
+}
--- sahajnanda/src/app/page.tsx
+++ sahajnanda/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <img src="/joella.bmp" alt="Logo" className="w-128 h-128" />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <img src="/joella.bmp" alt="Logo" className=" h-full" />
+      
     </div>
   );
 }
